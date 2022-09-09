19b4826dc2f9764aa6d9ba0502b30a2b73f3f950
netojav <nestoarana@gmail.com>
2022-09-08 18:35:29 -0600

    fix: always return a function

--- src/app/customers/services/customers-generator.factory.ts
+++ src/app/customers/services/customers-generator.factory.ts
@@ -1,9 +1,5 @@
-import { CUSTOMERS_COLLECTION } from '../constants';
 import { CustomersGeneratorService } from './customers-generator.service';
 
 export function customersGeneratorFactory(provider: CustomersGeneratorService) {
-  if (!localStorage[CUSTOMERS_COLLECTION]) {
-    return () => provider.generateRandomCustomers(100);
-  }
-  return null;
+  return () => provider.generateRandomCustomers(100);
 }
--- src/app/customers/services/customers-generator.service.ts
+++ src/app/customers/services/customers-generator.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { faker } from '@faker-js/faker';
+import { CUSTOMERS_COLLECTION } from '../constants';
 import { Customer, CustomerStatus } from '../types/customer';
+import { LocalStorageRefService } from './local-storage-ref.service';
 
 @Injectable()
 export class CustomersGeneratorService {
+  private localStorage: Storage;
+  constructor(private localStorageRefService: LocalStorageRefService) {
+    this.localStorage = localStorageRefService.localStorage;
+  }
   generateRandomCustomer(): Customer {
     const sex = faker.name.sexType();
     const firstName = faker.name.firstName(sex);
@@ -19,6 +25,14 @@ export class CustomersGeneratorService {
   }
 
   generateRandomCustomers(length = 20) {
-    return Array.from({ length }).map(_ => this.generateRandomCustomer());
+    if (!this.localStorage[CUSTOMERS_COLLECTION]) {
+      const customers = Array.from({ length }).map(_ =>
+        this.generateRandomCustomer()
+      );
+      this.localStorage.setItem(
+        CUSTOMERS_COLLECTION,
+        JSON.stringify(customers)
+      );
+    }
   }
 }
